ef567f1554faa30523fd94c383236c5802081a84
Isaac Park <isaac.a.park@gmail.com>
2015-11-12 01:29:41 -0500

    Example Code to Detect Cross Eye or Parallel

--- CrossViewFixer.py
+++ CrossViewFixer.py
@@ -1,10 +1,29 @@
-from imgurpython import ImgurClient
-from SimpleCV import *
-import praw
+from SimpleCV import Image, Display
 
-import config
-
-client = ImgurClient(config.imgur_client_id, config.imgur_client_secret)
+disp = Display()
 
 
+image = Image("https://c2.staticflickr.com/8/7508/16283544985_9875dac79d_b.jpg")
 
+image = image.scale(250.0/image.width)
+
+left = image.crop(0, 0, image.width/2, image.height)
+right = image.crop(image.width/2, 0, image.width/2, image.height)
+motion = left.findMotion(right)
+
+dx = 0
+for m in motion:
+    dx += m.dx
+
+print(dx)
+
+
+first = True
+
+while disp.isNotDone():
+        if disp.leftButtonDown:
+                first = not first
+        if first:
+            left.save(disp)
+        else:
+            right.save(disp)
