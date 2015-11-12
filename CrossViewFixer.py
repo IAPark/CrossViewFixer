7803e40700fafeb1d440b9c9905f123dfc19dece
Isaac Park <isaac.a.park@gmail.com>
2015-11-11 23:50:35 -0500

    Initial Structure

---
+++ CrossViewFixer.py
@@ -0,0 +1,10 @@
+from imgurpython import ImgurClient
+from SimpleCV import *
+import praw
+
+import config
+
+client = ImgurClient(config.imgur_client_id, config.imgur_client_secret)
+
+
+
---
+++ converter/CV.py
@@ -0,0 +1,5 @@
+from SimpleCV import Image
+import scipy
+
+image = Image("http://imgur.com/gallery/NXsM2XM")
+image.show()
