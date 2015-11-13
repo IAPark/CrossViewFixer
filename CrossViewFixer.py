3dac9d881848fcd7528c866ac0502defbd3be897
Isaac Park <isaac.a.park@gmail.com>
2015-11-12 23:52:41 -0500

    Calculate Accuracy Based on Summing Movement

--- CrossViewFixer.py
+++ CrossViewFixer.py
@@ -1,29 +1,45 @@
 from SimpleCV import Image, Display
-
-disp = Display()
-
-
-image = Image("https://c2.staticflickr.com/8/7508/16283544985_9875dac79d_b.jpg")
-
-image = image.scale(250.0/image.width)
-
-left = image.crop(0, 0, image.width/2, image.height)
-right = image.crop(image.width/2, 0, image.width/2, image.height)
-motion = left.findMotion(right)
-
-dx = 0
-for m in motion:
-    dx += m.dx
-
-print(dx)
+import training_set
 
 
-first = True
+def is_cross_view(url):
+    image = Image(url)
+    image = image.scale(200.0/image.width)
+    left = image.crop(0, 0, image.width/2, 300)
+    right = image.crop(image.width/2, 0, image.width/2, 300)
+    motion = left.findMotion(right)
+    dx = 0
+    for m in motion:
+        dx += m.dx * 3
+    print(len(motion))
+    return dx / float(len(motion))
 
-while disp.isNotDone():
-        if disp.leftButtonDown:
-                first = not first
-        if first:
-            left.save(disp)
+
+wrong = 0
+sum_wrong = 0
+sum_right = 0
+attempted = 0
+'''
+for u in training_set.p:
+    dx = is_cross_view(u)
+    if dx > 0:
+        wrong += 1
+        sum_wrong += dx
+        print(u + " doesn't compute right")
+    else:
+        sum_right += dx
+'''
+for u in training_set.c:
+        dx = is_cross_view(u)
+        attempted += 1
+        if dx < 0:
+            wrong += 1
+            sum_wrong += dx
+            print(u + " doesn't compute right")
         else:
-            right.save(disp)
+            sum_right += dx
+
+
+print(float(wrong)/attempted)
+print(sum_right/(attempted - float(wrong)))
+print(sum_wrong/float(wrong))
---
+++ training_set.py
@@ -0,0 +1,42 @@
+p = (
+    "http://i.imgur.com/u6YDCXA.png",
+    "http://imgur.com/B0X4OZ8.png",
+    "https://hatemybabymama.files.wordpress.com/2012/06/manifest.jpg",
+    "http://i.imgur.com/8HrVvzL.jpg",
+    "http://i.imgur.com/Rr6Sqx0.jpg",
+    "http://i.imgur.com/wwEokSu.jpg",
+    "http://i.imgur.com/a5FDoym.jpg",
+    "http://i.imgur.com/a5FDoym.jpg",
+    "http://i.imgur.com/YnmwYQU.png",
+    "http://i.imgur.com/JwfXj7N.png",
+    "http://i.imgur.com/unePcTA.jpg",
+    "http://i.imgur.com/1XV8tpj.jpg",
+    "http://i.imgur.com/HZ2KfLq.jpg",
+    "http://i.imgur.com/u6YDCXA.png"
+)
+
+c = (
+    "http://c2.staticflickr.com/4/3124/3165446406_552e6acdeb_n.jpg",
+    "http://i.imgur.com/xIpmaQ1.jpg",
+    "http://i.imgur.com/DnQVCyl.jpg",
+    "http://imgur.com/c3VFi5p.jpg",
+    "http://lastchancesalon.net/xview/barn-story-city.x.jpg",
+    "http://i.imgur.com/qy9F8g6.jpg",
+    "http://i.imgur.com/hXK1y5x.jpg",
+    "http://imgur.com/yGT3NZW.jpg",
+    "http://imgur.com/6vVoAFy.jpg",
+    "http://imgur.com/L6MjV3E.jpg",
+    "http://imgur.com/1PLktSj.jpg",
+    "http://c1.staticflickr.com/1/690/21068022272_dfbdb97782_n.jpg",
+    "http://i.imgur.com/LiX8mf9.jpg"
+)
+
+l = (
+    "Church.JPG",
+    "church1.JPG",
+    "example.JPG",
+    "example2.JPG",
+    "laying.JPG",
+    "scolture.JPG",
+    "st Pauls.JPG"
+)
